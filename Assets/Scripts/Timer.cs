fc350b6f56011477553c9e9a138707f74c4a8d15
nmakranyi <nmakrany@ucsc.edu>
2025-11-04 17:36:02 -0800

    added game loop object

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public TextMesh timerText;
+    public float startTime = 5f;
+
+    private float timeRemaining;
+    private bool isRunning = true;
+
+    void Start()
+    {
+        timeRemaining = startTime;
+        UpdateTimerText();
+    }
+
+    void Update()
+    {
+        if (!isRunning) return;
+
+        timeRemaining -= Time.deltaTime;
+
+        if (timeRemaining <= 0f)
+        {
+            timeRemaining = 0f;
+            isRunning = false;
+        }
+
+        UpdateTimerText();
+    }
+
+    void UpdateTimerText()
+    {
+        timerText.text = "Time: " + timeRemaining.ToString("F2") + "s";
+    }
+}
